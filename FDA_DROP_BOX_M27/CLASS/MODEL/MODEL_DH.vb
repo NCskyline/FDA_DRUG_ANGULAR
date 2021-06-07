b8ae4c1acc8a12e44019b2bf92df6b06a191ac53
Therdsak Bhummarin <therdsak.b@fsa.co.th>
2021-06-07 14:32:02 +0700

    test

--- FDA_DROP_BOX_M27/CLASS/MODEL/MODEL_DH.vb
+++ FDA_DROP_BOX_M27/CLASS/MODEL/MODEL_DH.vb
@@ -14,6 +14,21 @@
     Public EXP_DATE As Date
     Public _CER As New CER
 
-
+    ''' <summary>
+    ''' เลขที่ใบรับรอง (Certificate Number)
+    ''' </summary>
+    Public CERTIFICATION_NUMBER_ALL As String
+    ''' <summary>
+    ''' ชื่อสถานที่ผลิตในต่างประเทศ (Manufacturer) --CER_DETAIL_MANUFACTURE 
+    ''' </summary>
+    Public NAME_ADDRESS As String
+    ''' <summary>
+    ''' ที่อยู่ (Address) CER_DETAIL_MANUFACTURE
+    ''' </summary>
+    Public ADDRESS_NUMBER As String
+    ''' <summary>
+    ''' เมือง (City / Province/ State) --CER_DETAIL_MANUFACTURE 
+    ''' </summary>
+    Public ADDRESS_CITY As String
 
 End Class
