2ab92d17342e3c3b035f1721276b18ac25124f08
nnatthapakk <natthapak.t@outlook.com>
2021-10-26 15:08:23 +0700

    lcnlcn

---
+++ FDA_DROP_BOX_M27/Scripts_angular/LCN_STAFF_CTRL.js
@@ -0,0 +1,20 @@
+﻿app.controller('LCN_STAFF_CTRL', function ($scope, CENTER_SV, $http, $location) {
+    CHK_TOKEN();
+
+    Pageload();
+
+    function Pageload() {
+
+    }
+
+    $scope.LoadPreview = function () {
+        var MODLE_LCN = CENTER_SV.GET_LCN_INFORMATION_INPUT_V2(sessionStorage.CITIZEN_ID_AUTHORIZE, sessionStorage.LCT_IDA);
+        MODLE_LCN.then(function (datas) {
+
+            $scope.LIST_LCN = datas.data;
+            $scope.LIST_LCN.PROCESS = sessionStorage.PROCESS;
+            $scope.LIST_LCN.session = sessionStorage;
+            //$scope.LIST_LCN.PROCESS = "101";
+        }, function () { });
+    };
+});
