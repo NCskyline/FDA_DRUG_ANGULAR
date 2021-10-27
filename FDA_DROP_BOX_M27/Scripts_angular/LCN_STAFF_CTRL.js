a5ffdbf698600055567048f47bec4cd4afeb1411
nnatthapakk <natthapak.t@outlook.com>
2021-10-27 12:01:27 +0700

    script

--- FDA_DROP_BOX_M27/Scripts_angular/LCN_STAFF_CTRL.js
+++ FDA_DROP_BOX_M27/Scripts_angular/LCN_STAFF_CTRL.js
@@ -1,11 +1,14 @@
 ﻿app.controller('LCN_STAFF_CTRL', function ($scope, CENTER_SV, $http, $location) {
     CHK_TOKEN();
+    var LCT_IDA = sessionStorage.LCT_IDA;
+    var CITIZEN_ID_AUTHORIZE = sessionStorage.CITIZEN_ID_AUTHORIZE;
+    //var PROCESS = sessionStorage.PROCESS;
+    
+    //Pageload();
 
-    Pageload();
+    //function Pageload() {
 
-    function Pageload() {
-
-    }
+    //}
 
     $scope.LoadPreview = function () {
         var MODLE_LCN = CENTER_SV.GET_LCN_INFORMATION_INPUT_V2(sessionStorage.CITIZEN_ID_AUTHORIZE, sessionStorage.LCT_IDA);
